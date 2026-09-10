50c68674128e0e9ddbfb38a5e9bce7b42e3cdafd
Nathan Le Granvallet <nathan@legranvallet.fr>
2026-09-10 23:18:15 +0200

    Test

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import express from "express";
 
 // Env vars
 
-//const PRIM_API_KEY = process.env.API_KEY || "";
+const PRIM_API_KEY = process.env.API_KEY || "";
+const API_KEY = process.env.API_KEY || "";
 const PORT = process.env.PORT || "";
 
 // Express configuration
@@ -13,8 +14,16 @@ app.use(express.json());
 
 // ----- Main page ----------------------------------------------------------
 
-app.get("/", (_: express.Request, res: express.Response) => {
-  res.status(200).send("Welcome to the API");
+app.get("/", (req: express.Request, res: express.Response) => {
+  if (req.headers["authorization"] === API_KEY) {
+    res.status(200).send({
+      test: true,
+    });
+  } else {
+    res.status(401).send("Unauthorized");
+  }
+
+  return;
 });
 
 // ----- Healthcheck --------------------------------------------------------
