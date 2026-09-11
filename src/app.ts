6dc47fbd469dd12fabc5e3d698d1265ef4de9605
Nathan Le Granvallet <nathan@legranvallet.fr>
2026-09-11 15:44:03 +0200

    Ratp

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
 import express from "express";
-import { verifySecret } from "./helpers.ts";
+import { verifySecret, getStopTimes, getInfoTraffic } from "./helpers.ts";
 
 // Env vars
 
-const PRIM_API_KEY = process.env.PRIM_API_KEY || "";
+const METRO_12 = process.env.METRO_12 || "";
+const METRO_12_PORTE_DE_VERSAILLES =
+  process.env.METRO_12_PORTE_DE_VERSAILLES || "";
+
+const TRAM_T3A = process.env.TRAM_T3A || "";
+const TRAM_T3A_PORTE_DE_VERSAILLES =
+  process.env.TRAM_T3A_PORTE_DE_VERSAILLES || "";
+
+const METRO_14 = process.env.METRO_14 || "";
+
 const VERSION = process.env.GIT_TAG || "dev";
+
 const API_KEY = process.env.API_KEY || "";
 const PORT = process.env.PORT || "";
 
@@ -17,30 +27,48 @@ app.use(express.json());
 
 // ----- Main page ----------------------------------------------------------
 
-app.get("/", (req: express.Request, res: express.Response) => {
+app.get("/", async (req: express.Request, res: express.Response) => {
   if (!verifySecret(req.headers["authorization"] as string, API_KEY)) {
     console.log("Unauthorized");
     res.status(401).json({ message: "Unauthorized" });
     return;
   }
 
+  const metro12Stops = await getStopTimes(METRO_12_PORTE_DE_VERSAILLES);
+  const metro12Traffic = await getInfoTraffic(METRO_12);
+  const tramT3aStops = await getStopTimes(TRAM_T3A_PORTE_DE_VERSAILLES);
+  const tramT3aTraffic = await getInfoTraffic(TRAM_T3A);
+  const metro14Traffic = await getInfoTraffic(METRO_14);
+
   res.status(200).send({
     time: new Date().toISOString(),
     lines: {
       metro_12: {
-        next: [2, 4],
-        status: "running",
-        message: null,
+        next: metro12Stops.map((item: any) =>
+          item.expectedDepartureTime
+            ? Math.floor(
+                (item.expectedDepartureTime.getTime() - new Date().getTime()) /
+                  60000,
+              )
+            : null,
+        ),
+        terminus: metro12Stops.length > 0 ? metro12Stops[0].destination : null,
+        message: metro12Traffic,
       },
       tram_t3a: {
-        next: [null, null],
-        status: "stopped",
-        message: "La ligne est arrêtée en raison d'un incident.",
+        next: tramT3aStops.map((item: any) =>
+          item.expectedDepartureTime
+            ? Math.floor(
+                (item.expectedDepartureTime.getTime() - new Date().getTime()) /
+                  60000,
+              )
+            : null,
+        ),
+        terminus: tramT3aStops.length > 0 ? tramT3aStops[0].destination : null,
+        message: tramT3aTraffic,
       },
       metro_14: {
-        next: [16, 34],
-        status: "disrupted",
-        message: "La ligne est perturbée en raison d'un incident.",
+        message: metro14Traffic,
       },
     },
   });
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,8 @@
 import crypto from "crypto";
 
+const PRIM_API_URL = "https://prim.iledefrance-mobilites.fr/marketplace/";
+const PRIM_API_KEY = process.env.PRIM_API_KEY || "";
+
 /**
  * Verifies the webhook signature by comparing the received secret with the expected secret using a constant-time comparison to prevent timing attacks.
  *
@@ -24,4 +27,73 @@ export const verifySecret = (
   } catch {
     return false;
   }
+};
+
+export const getStopTimes = async (stopId: string) => {
+  const metroResponse = await fetch(
+    PRIM_API_URL + "stop-monitoring?MonitoringRef=" + stopId,
+    {
+      headers: {
+        apiKey: PRIM_API_KEY,
+      },
+    },
+  );
+  const metroData: any = await metroResponse.json();
+
+  const now = new Date();
+
+  const stopMonitoring =
+    metroData?.Siri?.ServiceDelivery?.StopMonitoringDelivery || [];
+
+  const times = stopMonitoring
+    .flatMap((delivery: any) => delivery.MonitoredStopVisit || [])
+    .map((visit: any) => {
+      const journey = visit.MonitoredVehicleJourney || {};
+      const call = journey.MonitoredCall || {};
+      const departureTimeStr = call.ExpectedDepartureTime;
+
+      return {
+        destination:
+          journey.DestinationName?.[0]?.value ||
+          call.DestinationDisplay?.[0]?.value,
+        expectedDepartureTime: departureTimeStr
+          ? new Date(departureTimeStr)
+          : null,
+      };
+    })
+    .filter(
+      (item: any) =>
+        item.expectedDepartureTime && item.expectedDepartureTime > now,
+    )
+    .sort(
+      (a: any, b: any) => a.expectedDepartureTime - b.expectedDepartureTime,
+    );
+  return times;
+};
+
+export const getInfoTraffic = async (stopId: string) => {
+  const response = await fetch(
+    PRIM_API_URL +
+      "general-message?LineRef=" +
+      stopId +
+      "&InfoChannelRef=Perturbation",
+    {
+      headers: {
+        apiKey: PRIM_API_KEY,
+      },
+    },
+  );
+  const data: any = await response.json();
+
+  const generalDeliveries =
+    data?.Siri?.ServiceDelivery?.GeneralMessageDelivery || [];
+
+  console.dir(generalDeliveries, { depth: null });
+
+  return generalDeliveries
+    .flatMap((delivery: any) => delivery.InfoMessage || [])
+    .flatMap((info: any) => info.Content?.Message || [])
+    .filter((msg: any) => msg.MessageType === "SHORT_MESSAGE")
+    .filter((msg: any) => msg.MessageText?.lang === "fr")
+    .map((msg: any) => msg.MessageText?.value);
 };
