7cedff4706f58bd1e81008dfe60c178938ac0412
Nathan Le Granvallet <nathan@legranvallet.fr>
2026-09-20 13:33:29 +0200

    Battery

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express from "express";
-import { verifySecret, getStopTimes, getInfoTraffic } from "./helpers.ts";
+import {
+  verifySecret,
+  getStopTimes,
+  getInfoTraffic,
+  loginTerminus,
+  refreshTerminusToken,
+  getKindleBattery,
+} from "./helpers.ts";
 
 // Env vars
 
@@ -13,6 +20,8 @@ const TRAM_T3A_PORTE_DE_VERSAILLES =
 
 const METRO_14 = process.env.METRO_14 || "";
 
+const TERMINUS_DEVICE = process.env.TERMINUS_DEVICE || "";
+
 const VERSION = process.env.GIT_TAG || "dev";
 
 const API_KEY = process.env.API_KEY || "";
@@ -25,6 +34,9 @@ app.disable("x-powered-by");
 app.use(express.static(VERSION === "dev" ? "public" : "/app/dist/public"));
 app.use(express.json());
 
+let accessToken = "";
+let refreshToken = "";
+
 // ----- Main page ----------------------------------------------------------
 
 app.get("/", async (req: express.Request, res: express.Response) => {
@@ -34,11 +46,48 @@ app.get("/", async (req: express.Request, res: express.Response) => {
     return;
   }
 
+  // Ensure we have valid Terminus access and refresh tokens before proceeding
+
+  if (!accessToken) {
+    try {
+      ({ accessToken, refreshToken } = await loginTerminus());
+    } catch (error) {
+      console.log("Failed to log in to Terminus:", error);
+      res.status(500).json({ message: "Failed to log in to Terminus" });
+      return;
+    }
+  } else {
+    try {
+      ({ accessToken, refreshToken } = await refreshTerminusToken(
+        accessToken,
+        refreshToken,
+      ));
+    } catch (error) {
+      console.log("Failed to refresh Terminus token:", error);
+      res.status(500).json({ message: "Failed to refresh Terminus token" });
+      return;
+    }
+  }
+
+  // Get Kindle battery
+
+  let battery = null;
+
+  try {
+    battery = await getKindleBattery(TERMINUS_DEVICE, accessToken);
+  } catch (error) {
+    console.log("Failed to retrieve Kindle battery:", error);
+  }
+
+  // Retrieve stop times and traffic information for the specified metro and tram lines
+
   const metro12Stops = await getStopTimes(METRO_12_PORTE_DE_VERSAILLES);
   const metro12Traffic = await getInfoTraffic(METRO_12);
   const tramT3aStops = await getStopTimes(TRAM_T3A_PORTE_DE_VERSAILLES);
   const tramT3aTraffic = await getInfoTraffic(TRAM_T3A);
   const metro14Traffic = await getInfoTraffic(METRO_14);
+
+  // Send the response with the retrieved information
 
   res.status(200).send({
     time: new Date().toISOString(),
@@ -71,6 +120,7 @@ app.get("/", async (req: express.Request, res: express.Response) => {
         message: metro14Traffic,
       },
     },
+    battery: battery,
   });
 });
 
--- src/helpers.ts
+++ src/helpers.ts
@@ -3,6 +3,10 @@ import crypto from "crypto";
 const PRIM_API_URL = "https://prim.iledefrance-mobilites.fr/marketplace/";
 const PRIM_API_KEY = process.env.PRIM_API_KEY || "";
 
+const TERMINUS_API_URL = process.env.TERMINUS_API_URL || "";
+const TERMINUS_LOGIN = process.env.TERMINUS_LOGIN || "";
+const TERMINUS_PASSWORD = process.env.TERMINUS_PASSWORD || "";
+
 /**
  * Verifies the webhook signature by comparing the received secret with the expected secret using a constant-time comparison to prevent timing attacks.
  *
@@ -29,6 +33,12 @@ export const verifySecret = (
   }
 };
 
+/**
+ * Retrieves the upcoming departure times for a given stop.
+ *
+ * @param stopId The ID of the stop for which to retrieve the upcoming departure times.
+ * @returns An array of objects containing the destination and expected departure time for each upcoming vehicle.
+ */
 export const getStopTimes = async (stopId: string) => {
   const metroResponse = await fetch(
     PRIM_API_URL + "stop-monitoring?MonitoringRef=" + stopId,
@@ -71,6 +81,12 @@ export const getStopTimes = async (stopId: string) => {
   return times;
 };
 
+/**
+ * Retrieves the traffic information for a given stop.
+ *
+ * @param stopId The ID of the stop for which to retrieve traffic information.
+ * @returns An array of short messages describing the current traffic conditions.
+ */
 export const getInfoTraffic = async (stopId: string) => {
   const response = await fetch(
     PRIM_API_URL +
@@ -88,12 +104,97 @@ export const getInfoTraffic = async (stopId: string) => {
   const generalDeliveries =
     data?.Siri?.ServiceDelivery?.GeneralMessageDelivery || [];
 
-  console.dir(generalDeliveries, { depth: null });
-
   return generalDeliveries
     .flatMap((delivery: any) => delivery.InfoMessage || [])
     .flatMap((info: any) => info.Content?.Message || [])
     .filter((msg: any) => msg.MessageType === "SHORT_MESSAGE")
     .filter((msg: any) => msg.MessageText?.lang === "fr")
     .map((msg: any) => msg.MessageText?.value);
+};
+
+/**
+ * Logs in to the Terminus system and retrieves access and refresh tokens.
+ *
+ * @returns An object containing the access and refresh tokens.
+ */
+export const loginTerminus = async () => {
+  const response = await fetch(TERMINUS_API_URL + "login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      login: TERMINUS_LOGIN,
+      password: TERMINUS_PASSWORD,
+    }),
+  });
+  const data: any = await response.json();
+
+  if (data?.access_token && data?.refresh_token) {
+    return {
+      accessToken: data.access_token,
+      refreshToken: data.refresh_token,
+    };
+  } else {
+    throw new Error("Failed to log in to Terminus");
+  }
+};
+
+/**
+ * Refreshes the Terminus access token using the provided refresh token.
+ *
+ * @param accessToken The current access token used for authorization.
+ * @param refreshToken The refresh token used to obtain a new access token.
+ * @returns An object containing the new access and refresh tokens.
+ */
+export const refreshTerminusToken = async (
+  accessToken: string,
+  refreshToken: string,
+) => {
+  const response = await fetch(TERMINUS_API_URL + "api/jwt", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: accessToken,
+    },
+    body: JSON.stringify({
+      refresh_token: refreshToken,
+    }),
+  });
+  const data: any = await response.json();
+
+  if (data?.access_token && data?.refresh_token) {
+    return {
+      accessToken: data.access_token,
+      refreshToken: data.refresh_token,
+    };
+  } else {
+    throw new Error("Failed to refresh Terminus token");
+  }
+};
+
+/**
+ * Retrieves the battery charge of a Kindle device.
+ *
+ * @param deviceId The ID of the Kindle device.
+ * @param accessToken The access token used for authorization.
+ * @returns The battery charge of the Kindle device.
+ */
+export const getKindleBattery = async (
+  deviceId: string,
+  accessToken: string,
+) => {
+  const response = await fetch(TERMINUS_API_URL + "api/devices/" + deviceId, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: accessToken,
+    },
+  });
+  const data: any = await response.json();
+
+  if (data?.data?.battery_charge) {
+    return data.data.battery_charge;
+  } else {
+    throw new Error("Failed to retrieve Kindle battery");
+  }
 };
